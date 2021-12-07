d2270b4a85d4918ef538cc73ea36ac3c33d7f394
Topherian <Topherian@users.noreply.github.com>
2021-12-07 17:49:20 +0000

    Update Day2Tests.scala

--- adventOfCode/app/src/test/scala/com/topherian/adventOfCode/Day2Tests.scala
+++ adventOfCode/app/src/test/scala/com/topherian/adventOfCode/Day2Tests.scala
@@ -14,9 +14,26 @@ class Day2Tests extends AnyFunSuite {
 
   test("Calculate depth with Sample input") {
     val fileStream = getClass.getResourceAsStream("/Day2Sample.txt")
-    val lines: Map[String, String] = Source.fromInputStream(fileStream).getLines().toList.map((s: String) => s.split("\\ ").toList)
-      .collect{case key :: value :: _ => key -> value}.toMap
-    for(line<-lines)
-      println(line)
+    val lines: List[Array[String]] = Source.fromInputStream(fileStream).getLines().toList.map((s: String) => s.split("\\ "))
+    assert(Day2.depthCalculator(lines) == 150)
   }
+
+  test("Calculate depth with Actual input") {
+    val fileStream = getClass.getResourceAsStream("/Day2Actual.txt")
+    val lines: List[Array[String]] = Source.fromInputStream(fileStream).getLines().toList.map((s: String) => s.split("\\ "))
+    assert(Day2.depthCalculator(lines) == 2073315)
+  }
+
+  test("Calculate depth with aim with Sample input") {
+    val fileStream = getClass.getResourceAsStream("/Day2Sample.txt")
+    val lines: List[Array[String]] = Source.fromInputStream(fileStream).getLines().toList.map((s: String) => s.split("\\ "))
+    assert(Day2.depthCalculatorWithAim(lines) == 900)
+  }
+
+  test("Calculate depth with aim with Actual input") {
+    val fileStream = getClass.getResourceAsStream("/Day2Actual.txt")
+    val lines: List[Array[String]] = Source.fromInputStream(fileStream).getLines().toList.map((s: String) => s.split("\\ "))
+    assert(Day2.depthCalculatorWithAim(lines) == 1840311528)
+  }
+
 }
