57b37b1afc8fa81cb0c185f612225b58226ffc51
Christopher Bautista <Topherian@users.noreply.github.com>
2021-12-08 20:37:37 +0000

    Tidy up

--- adventOfCode/app/src/main/scala/com/topherian/adventOfCode/Day3.scala
+++ adventOfCode/app/src/main/scala/com/topherian/adventOfCode/Day3.scala
@@ -1,5 +1,55 @@
 package com.topherian.adventOfCode
 
-class Day3 {
+import scala.io.Source
 
+object Day3 {
+
+  def main(args: Array[String]): Unit = {
+    val fileStream = getClass.getResourceAsStream("/Day3Actual.txt")
+    val input = Source.fromInputStream(fileStream).getLines.toList
+    computePower(input)
+  }
+
+  def computePower(input: List[String]): Int = {
+    val binaryMap:scala.collection.mutable.Map[Int,Int] = scala.collection.mutable.Map[Int, Int]()
+    val inputSize = input.size / 2
+    for(line<-input) {
+      var column = 0
+      for(c<-line) {
+        var currentCount: Int = binaryMap.getOrElse(column, 0)
+        if (c.equals('1')) {
+          currentCount += 1
+          binaryMap.put(column,currentCount)
+        } else {
+          binaryMap.put(column,currentCount)
+        }
+        column+=1
+      }
+    }
+    val gammaBinary = generateBinary(binaryMap,inputSize)
+    val gamma = Integer.parseInt(gammaBinary,2)
+    val epsilon =Integer.parseInt(flipBinary(gammaBinary), 2)
+    gamma*epsilon
+  }
+
+  def generateBinary(binaryMap: scala.collection.mutable.Map[Int,Int], threshold: Int): String = {
+    val binaryNumber = new StringBuilder()
+    binaryMap.foreach(count =>
+      if (count._2 > threshold) {
+        binaryNumber.append("1")
+      }
+      else {
+        binaryNumber.append("0")
+      }
+    )
+    binaryNumber.toString()
+  }
+
+  def flipBinary(binaryString: String): String = {
+    val flipped = binaryString
+    val flipped1 = flipped.replace('1', 'x').
+      replace('0','1').
+      replace('x', '0')
+    flipped1
+  }
 }
--- adventOfCode/app/src/test/scala/com/topherian/adventOfCode/Day3Tests.scala
+++ adventOfCode/app/src/test/scala/com/topherian/adventOfCode/Day3Tests.scala
@@ -9,58 +9,16 @@ import scala.io.Source
 @RunWith(classOf[JUnitRunner])
 class Day3Tests extends AnyFunSuite {
 
-  test("Calculate diagnostic with Sample input") {
-    val fileStream = getClass.getResourceAsStream("/Day3SampleSmall.txt")
+  test("Calculate power with Sample input") {
+    val fileStream = getClass.getResourceAsStream("/Day3Sample.txt")
     val input = Source.fromInputStream(fileStream).getLines.toList
-    val binaryMap:scala.collection.mutable.Map[Int,Int] = scala.collection.mutable.Map[Int, Int]()
-    val inputSize = input.size / 2
-    for(line<-input) {
-      var column = 0
-      for(c<-line) {
-        var currentCount: Int = binaryMap.getOrElse(column, 0)
-        if (c.equals('1')) {
-          currentCount += 1
-          binaryMap.put(column,currentCount)
-        } else {
-          binaryMap.put(column,currentCount)
-        }
-        column+=1
-      }
-    }
-    assert(Integer.parseInt(generateBinary(binaryMap,inputSize),2) == 22)
+    assert(Day3.computePower(input) == 198)
   }
 
-  test("Calculate diagnostic with Actual input") {
+  test("Calculate power with Actual input") {
     val fileStream = getClass.getResourceAsStream("/Day3Actual.txt")
     val input = Source.fromInputStream(fileStream).getLines.toList
-    val binaryMap:scala.collection.mutable.Map[Int,Int] = scala.collection.mutable.Map[Int, Int]()
-    val inputSize = input.size / 2
-    for(line<-input) {
-      var column = 0
-      for(c<-line) {
-        var currentCount: Int = binaryMap.getOrElse(column, 0)
-        if (c.equals('1')) {
-          currentCount += 1
-          binaryMap.put(column,currentCount)
-        } else {
-          binaryMap.put(column,currentCount)
-        }
-        column+=1
-      }
-    }
-    assert(Integer.parseInt(generateBinary(binaryMap,inputSize),2) == 22)
+    assert(Day3.computePower(input) == 2967914)
   }
 
-  def generateBinary(binaryMap: scala.collection.mutable.Map[Int,Int], threshold: Int): String = {
-    val binaryNumber = new StringBuilder()
-    binaryMap.foreach(count =>
-      if (count._2 > threshold) {
-        binaryNumber.append("1")
-      }
-      else {
-        binaryNumber.append("0")
-      }
-    )
-    binaryNumber.toString()
-  }
 }
